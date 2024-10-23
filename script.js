aaee1a2973baef5b8f775803db16a90479c8b7ca
Marie Marchal <you@example.com>
2024-10-23 10:34:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Etape 1, créer une fonction qui demande un chiffre à l'utilisateur
+
+function demanderChiffre() {
+    let chiffre = prompt("Donnez un nombre au hasard :");
+    let givenNumber = Number(chiffre)
+    return givenNumber
+}
+
+//demanderChiffre()
+
+
+// Etape 2, créer une fonction avec condition pour deviner 22
+// + étape 3 avec fonction gamePlay
+
+function didIWin(givenNumber) {
+    const keyNumber = 22
+
+    if (givenNumber < keyNumber) {
+        alert("Plus grand !")
+    } else if (givenNumber > keyNumber) {
+        alert("Plus petit !")
+    } else {
+        alert("Bravo ! Tu as deviné le chiffre !")
+        return true
+    }
+    return false
+}
+
+// lorsque l'utilisateur n'a pas trouvé le nombre il recommance, une fois trouvé le nombre, la boucle s'arrete (while)
+
+function gamePlay() {
+    let found = false;
+    do {
+        let userGuess = demanderChiffre();
+        found = didIWin(userGuess);
+    }
+    while (!found);
+}
+
+gamePlay();
+
